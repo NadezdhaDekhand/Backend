03d12094c87bd37158c6c5bb84ad692733b0ae67
NadezdhaDekhand <nadeshka7979@gmail.com>
2022-01-29 20:08:27 +0700

    ДЗ №4

---
+++ src/main/java/HomeWork_4/UserFollow.java
@@ -0,0 +1,11 @@
+package HomeWork_4;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class UserFollow {
+    @JsonProperty("status")
+    public Boolean status;
+
+}
