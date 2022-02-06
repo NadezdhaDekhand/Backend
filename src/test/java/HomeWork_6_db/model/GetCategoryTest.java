b613d51a66a1a34108e90593478316058d1bd318
NadezdhaDekhand <nadeshka7979@gmail.com>
2022-02-06 23:07:02 +0700

    ДЗ №6

---
+++ src/test/java/HomeWork_6_db/model/GetCategoryTest.java
@@ -0,0 +1,41 @@
+package HomeWork_6_db.model;
+
+import HomeWork_5.*;
+import HomeWork_6_db.dao.ProductsMapper;
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.*;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetCategoryTest {
+
+    static CategoryService categoryService;
+
+    @BeforeAll
+    static void beforeAll() {
+        categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryByIdPositiveTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(100).execute();
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+
+
+    }
+
+}
